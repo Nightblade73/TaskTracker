98f254b6417b2fbd8d6eba73687a189c286069b8
Nightblade73 <v-zar@list.ru>
2018-12-01 23:47:21 +0400

    Добавлена возможность добавление заданий визуально

--- app/views/tasklist_view.php
+++ app/views/tasklist_view.php
@@ -8,7 +8,7 @@
             Авторизован менеджер!!
         <?php endif; ?>
     </a>
-    <button  type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+    <button  type="button" class="btn btn-primary" data-toggle="modal" data-target="#add-Task">
         Добавить задачу
     </button>
     <form class="form-inline float-right">
@@ -19,33 +19,16 @@
 </nav>
 
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
-                <button id="add-task" type="button" class="btn btn-primary">Добавить задачу</button>
-            </div>
-        </div>
-    </div>
-</div>
 <div class="container-fluid">
     <div class="card text-white bg-primary mb-3 " style="max-width: 18rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body">
-            <h5 class="card-title">Primary card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
+        <button type="button" class="card-header btn btn-primary" data-toggle="modal" data-target="#edit-Task"> 
+            Имя задачи 
+        </button>
+    </div>
+    <div class="card text-white bg-primary mb-3 " style="max-width: 18rem;">
+        <button type="button" class="card-header btn btn-primary" data-toggle="modal" data-target="#edit-Task"> 
+            Имя задачи 
+        </button>
     </div>
     <!--                <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
                         <div class="card-header">Header</div>
@@ -97,5 +80,67 @@
                         </div>
                     </div>-->
 
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="add-Task" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="text">Имя задачи:</label>
+                        <input id="task-name" type="text" class="form-control">
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                    <button id="add" type="submit" class="btn btn-primary" data-dismiss="modal">Добавить</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="edit-Task" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="">
+                    <div class="form-group">
+                        <label for="email">Email address:</label>
+                        <input type="email" class="form-control" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd">Password:</label>
+                        <input type="password" class="form-control" id="pwd">
+                    </div>
+                    <div class="form-group form-check">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox"> Remember me
+                        </label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                <button id="" type="button" class="btn btn-primary">Добавить задачу</button>
+            </div>
+        </div>
+    </div>
 </div>
 
--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
 
 
-$('#add-task').click(function (event) {
-    addDynamicExtraField();
+//$('#add').click(function (event) {
+//    console.log('я здесь');
+//    event.preventDefault();
+//    addDynamicExtraField();
+//});
+
+$(document).on('click', '#add', function (e)
+{
+    e.preventDefault();
+    var $tasks = $('.container-fluid');
+    console.log($('#task-name').val());
+    $('.container-fluid').append('<div class="card text-white bg-primary mb-3 " style="max-width: 18rem;">\n\
+        <button type="button" class="card-header btn btn-primary" data-toggle="modal"data-target="#editTask"> ' +
+           $('#task-name').val() + '</button>\n\
+        </div>');
 });
 
-function addDynamicExtraField() {
-    var $tables = $('.container-fluid');
-    for (var i = 1; i <= 15; i++) {
-        $('<div />', {class: 'inner', text: i}).appendTo($tables);
-    };
-}
+//
+//$(function()
+//{
+//    $(document).on('click', '.add-task', function(e)
+//    {
+//        e.preventDefault();
+//
+//        var controlForm = $('.controls form:first'),
+//            currentEntry = $(this).parents('.entry:first'),
+//            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+//
+//        newEntry.find('input').val('');
+//        controlForm.find('.entry:not(:last) .btn-add')
+//            .removeClass('btn-add').addClass('btn-remove')
+//            .removeClass('btn-success').addClass('btn-danger')
+//            .html('<span class="glyphicon glyphicon-minus"></span>');
+//    }).on('click', '.btn-remove', function(e)
+//    {
+//		$(this).parents('.entry:first').remove();
+//
+//		e.preventDefault();
+//		return false;
+//	});
+//});
