45f6c07a0bba2f195f46056bcd1928ea38794334
Nightblade73 <v-zar@list.ru>
2018-12-05 12:55:15 +0400

    исправлена разметка регистрации

--- app/views/signup_view.php
+++ app/views/signup_view.php
@@ -1,14 +1,9 @@
 <form action="/signup/registration" method="POST">
-    <p>
     <p> <strong>Ваш логин</strong>:</p>
     <input type="text" name="login" value="<?php echo $data['login']; ?>">
-    </p>
-    <p>
     <p> <strong>Ваш e-mail</strong>:</p>
     <input type="email" name="email" value="<?php echo $data['email']; ?>">
-    </p>
-    <p>
-    <p>
+
     <p> <strong>Ваша роль</strong>:</p>
     <select name="role">
         <?php
@@ -18,16 +13,13 @@
         }
         ?>
     </select>
-</p>
-<p>
-<p>  <strong>Ваш пароль</strong>:</p>
-<input type="password" name="password">
-</p>
-<p>
-<p>  <strong>Введите пароль повторно</strong>:</p>
-<input type="password" name="password_сonfirm">
-</p>
-<p>
-    <button type="submit" name="do_signup">Зарегистрироваться</button>
-</p>
+
+    <p>  <strong>Ваш пароль</strong>:</p>
+    <input type="password" name="password">
+
+    <p>  <strong>Введите пароль повторно</strong>:</p>
+    <input type="password" name="password_сonfirm">
+    <p>
+        <button type="submit" name="do_signup">Зарегистрироваться</button>
+    </p>
 </form>
